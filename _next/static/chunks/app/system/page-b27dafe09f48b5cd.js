ee581c2b0c251d4baf7c45b030e11c37d4e96690
Egor <davegor2015@gmail.com>
2026-09-24 14:11:37 +0300

    Add animated SKVERNA 3D crest with static fallback

---
+++ _next/static/chunks/app/system/page-b27dafe09f48b5cd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[186,714],{6587:(e,s,n)=>{Promise.resolve().then(n.bind(n,2778)),Promise.resolve().then(n.t.bind(n,9970,23))}},e=>{e.O(0,[59,919,156,358],()=>e(e.s=6587)),_N_E=e.O()}]);
